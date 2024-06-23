0b375e053de6a3f3ecc2be4433d71a3cacfbff94
Vaalarivan2002 <pvaalarivan@gmail.com>
2024-06-23 18:00:12 +0530

    changed question model

--- api/chatbot/questionsApp/models.py
+++ api/chatbot/questionsApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Question(models.Model):
     question_id = models.AutoField(auto_created=True, primary_key=True)
-    question = models.TextField(unique=True)
+    question = models.TextField()
     answer = models.TextField()
     
 # Create log table
